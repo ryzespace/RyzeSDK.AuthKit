883a0c78722c37398e3796b591f5a8d0ad13f6a8
Lunix <kenny6224@protonmail.com>
2025-11-22 23:08:24 +0100

    Add HostBuilder extension for AuthKit.Console
    Introduces HostBuilderExtensions with UseAuthKitConsole to configure services for AuthKit.Console, including registration of ServerInitializer as a singleton.

---
+++ Integration/AuthKit.Console/Configuration/HostBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthKit.Console.Configuration;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder UseAuthKitConsole(this IHostBuilder builder)
+    {
+        return builder.ConfigureServices((context, services) =>
+        {
+            services.ConfigureApp(context.Configuration);
+        });
+    }
+
+    private static IServiceCollection ConfigureApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<ServerInitializer>();
+        return services;
+    }
+}
